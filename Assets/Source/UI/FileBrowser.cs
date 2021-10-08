494ff7b472b2bd5f50226c7e8ead93c87f729fc0
Blade12629 <blade1234@web.de>
2021-10-08 20:15:53 +0200

    Made ui functional, some bugfixes

---
+++ Assets/Source/UI/FileBrowser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using SimpleFileBrowser;
+using FB = SimpleFileBrowser.FileBrowser;
+using System.Collections;
+
+namespace Assets.Source.UI
+{
+    public static class FileBrowser
+    {
+        public static void OpenFile(string title, Action<string> onSelected, Action onCancelled)
+        {
+            FB.ShowLoadDialog(new FB.OnSuccess(s => InvokeSelected(s, onSelected)), new FB.OnCancel(onCancelled), FB.PickMode.Files, title: title);
+        }
+
+        public static void SaveFile(string title, Action<string> onSelected, Action onCancelled)
+        {
+            FB.ShowSaveDialog(new FB.OnSuccess(s => InvokeSelected(s, onSelected)), new FB.OnCancel(onCancelled), FB.PickMode.Files, title: title);
+        }
+
+        public static void SelectFolder(string title, Action<string> onSelected, Action onCancelled)
+        {
+            FB.ShowLoadDialog(new FB.OnSuccess(s => InvokeSelected(s, onSelected)), new FB.OnCancel(onCancelled), FB.PickMode.Folders, title: title);
+        }
+
+        static void InvokeSelected(string[] s, Action<string> onSelected)
+        {
+            if (s == null || s.Length == 0)
+                return;
+
+            onSelected?.Invoke(s[0]);
+        }
+    }
+}
---
+++ Assets/Source/UI/GenerateFlatlandMapPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Source.UI
+{
+    public class GenerateFlatlandMapPanel : MonoBehaviour
+    {
+        [SerializeField] InputField _widthInput;
+        [SerializeField] InputField _depthInput;
+
+        public void GenerateFlatlandMap()
+        {
+            if (!int.TryParse(_widthInput.text, out int w) || !int.TryParse(_depthInput.text, out int d))
+                return;
+
+            gameObject.SetActive(false);
+            Client.Instance.LoadMap(null, w, d, Game.GameMap.GenerationOption.Flatland, null, null);
+        }
+    }
+}
