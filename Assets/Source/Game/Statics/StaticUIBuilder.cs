ef68d00917935e5a87399b729285dae032e03ea7
Blade12629 <blade1234@web.de>
2021-10-08 13:21:42 +0200

    Prepare static builder placeholder class for ui

---
+++ Assets/Source/Game/Statics/StaticUIBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Source.Game.Statics
+{
+    public class StaticUIBuilder : MonoBehaviour // this will be the editor tool used to construct the StaticInfo's through 2d image and 3d mesh
+    {
+    }
+}
