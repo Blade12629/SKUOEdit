0bbee836cb91cef00561a2bbe4945eb40ce449e5
Blade12629 <blade1234@web.de>
2021-10-10 10:50:27 +0200

    Added Input wrapper for UnityEngine.Input

--- Assets/Source/Input.cs
+++ Assets/Source/Input.cs
@@ -10,6 +10,12 @@ using IP = UnityEngine.Input;
 
 namespace Assets.Source
 {
+    /// <summary>
+    /// This class is a wrapper around <see cref="UnityEngine.Input"/>
+    /// <para>You can change any key to another by simply replacing it</para>
+    /// <para>e.g. you want Key2 to count as Key1: ReplaceKey(Key2, Key1)</para>
+    /// <para>e.g. you want Key1 to count as Key2 and vice versa: ReplaceKey(Key1, Key2) ReplaceKey(Key2, Key1)</para>
+    /// </summary>
     public static class Input
     {
         public static Vector2 mouseScrollDelta => IP.mouseScrollDelta;
@@ -29,6 +35,13 @@ namespace Assets.Source
         public static bool GetKeyUp(KeyCode key)
             => IP.GetKeyUp(ReplaceKey(key));
 
+        public static bool GetKeyUnreplaced(KeyCode key)
+            => IP.GetKey(key);
+        public static bool GetKeyDownUnreplaced(KeyCode key)
+            => IP.GetKeyDown(key);
+        public static bool GetKeyUpUnreplaced(KeyCode key)
+            => IP.GetKeyUp(key);
+
         public static bool GetMouseButton(int button)
             => IP.GetMouseButton(button);
         public static bool GetMouseButtonDown(int button)
@@ -42,9 +55,9 @@ namespace Assets.Source
         public static float GetAxisRaw(string axisName)
             => IP.GetAxisRaw(axisName);
 
-        public static void ReplaceKey(KeyCode original, KeyCode replaced)
+        public static void ReplaceKey(KeyCode original, KeyCode replacement)
         {
-            _replacedKeys[original] = replaced;
+            _replacedKeys[original] = replacement;
         }
 
         public static void DeleteReplacement(KeyCode original)
